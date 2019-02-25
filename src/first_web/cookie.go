35451f5fd602866c52ce813221785379bcc976d2
rock <1277239178@qq.com>
2019-02-25 09:45:36 -0800

    change same code

--- src/first_web/cookie.go
+++ src/first_web/cookie.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -15,13 +16,23 @@ func setCookie(w http.ResponseWriter, r *http.Request) {
 		Value:    "Manning Co",
 		HttpOnly: true,
 	}
-	w.Header().Set("Set-cookie", c1.String())
-	w.Header().Set("Set-cookie", c2.String())
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "cannot get the first cookie")
+	}
+	cs := r.Cookies()
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
 }
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:9091",
 	}
 	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
 	server.ListenAndServe()
 }
